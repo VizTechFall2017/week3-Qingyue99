5ddf862f865156e475b4a2f0a0fa7eff9d99df30
Qingyue99 <31904741+Qingyue99@users.noreply.github.com>
2017-09-29 19:18:32 -0400

    HW3

--- PS02/index.js
+++ PS02/index.js
@@ -1,3 +1,34 @@
 var svg = d3.select('svg');
 
 /* Your code goes here */
+
+
+d3.csv('Qingyuesample1.csv', function(dataIn) {
+    console.log(dataIn);
+
+    svg.selectAll('circle')
+        .data(dataIn)
+        .enter()
+        .append('circle')
+        .attr('cx', function (d) {
+            console.log(d.x);
+            return d.x;
+
+        })
+
+        .attr('cy', function (d) {
+            return d.y;
+
+        })
+
+        .attr('r', function (d) {
+            return d.r;
+
+        })
+
+        .attr('fill', function (d) {
+            return d.fill;
+        })
+});
+
+
